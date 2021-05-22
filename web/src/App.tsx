eb8d57b6705c1197ff0077ebaa4f19813315af9f
Nye Evans <nyeevans@outlook.com>
2021-05-22 01:36:20 +0100

    literally just a comment

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,11 +4,14 @@ import "./App.css";
 import Category from "./components/category";
 
 function App() {
+
+    // how can we store a list of categories?
+
     return (
         <div className="home-page">
             <div className="categoryContainer">
                 <Category name="test" />
-                
+                <Category name="test" />
                 <button className = "category">+</button>
             </div>
         </div>
