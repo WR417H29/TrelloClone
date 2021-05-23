3637d4ba2a2473cbeb71181415d37b316fc23e15
Wraith <i.acnaylor@gmail.com>
2021-05-23 10:24:58 +0100

    Added routes to allow the frontend to retrieve the data from the API

--- server/api/routes.py
+++ server/api/routes.py
@@ -7,16 +7,46 @@ from api.models import Board, Category, Card
 @app.route('/boards', methods=['GET', 'POST'])
 def boards():
     if request.method == 'GET':
-        print("GET Request Receieved")
+        print("GET Boards")
 
         boards = []
         for x in Board.query.all():
             boards.append({
-                'board_id': x.id,
-                'board_name': x.name
+                'id': x.id,
+                'name': x.name
             })
 
         return jsonify(boards), 200
 
-    elif request.method == 'POST':
-        pass
+
+@app.route('/categories', methods=['GET', 'POST'])
+def categories():
+    if request.method == 'GET':
+        print('GET Categories')
+
+        categories = []
+        for x in Category.query.all():
+            categories.append({
+                'id': x.id,
+                'name': x.name,
+                'boardID': x.board_id
+            })
+
+        return jsonify(categories), 200
+
+
+@app.route('/cards', methods=['GET', 'POST'])
+def cards():
+    if request.method == 'GET':
+        print('GET Cards')
+
+        cards = []
+        for x in Card.query.all():
+            cards.append({
+                'id': x.id,
+                'name': x.name,
+                'body': x.body,
+                'categoryID': x.category_id
+            })
+
+        return jsonify(cards), 200
