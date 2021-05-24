a98699ec5caf53307f3c5ff7176502d067993fa8
atlastrinity <nyeevans@outlook.com>
2021-05-24 14:57:35 +0100

    started adding board menu

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { textChangeRangeIsUnchanged } from "typescript";
 import "./App.css";
 
 import Board from "./components/board";
@@ -8,6 +9,7 @@ interface appProps {}
 
 interface appState {
     boards: [];
+    currentBoardID: number;
 } // declaring the data which must be in the app state
 
 class App extends React.Component<appProps, appState> {
@@ -15,7 +17,16 @@ class App extends React.Component<appProps, appState> {
         super(props);
         this.state = {
             boards: [],
+            currentBoardID: -1, // -1 signifies no board has been selected yet
         }; // creating an empty list
+    }
+
+    setBoard(toSet:BoardType){ // used to set the current board being viewed
+        this.setState({currentBoardID: toSet.id});
+    }
+
+    resetBoard(){
+        this.setState({currentBoardID: -1})
     }
 
     componentDidMount() {
@@ -31,17 +42,34 @@ class App extends React.Component<appProps, appState> {
     }
 
     render() {
-        let disp: any = [];
-        this.state.boards.forEach((board: BoardType) => {
+
+        if (this.state.currentBoardID == -1){ // if no board is selected then give the user a menu
+            let disp:any = [];
+            this.state.boards.forEach((board: BoardType) => {
+                disp.push(
+                    <button onClick = {() => this.setBoard(board)}>{board.name}</button>
+                )
+            });
+            return(
+            <>
+                {disp}
+            </>
+            )
+        }
+        else{
+            let disp: any = [];
+            this.state.boards.forEach((board: BoardType) => {
             disp.push(
                 <div className="board">
                     <p>{board.name}: </p>
-                    <Board name={board.name} id={board.id} key={board.id} />
+                    <Board name={board.name} id={board.id} key={board.id} menuFunction = {() => this.resetBoard()} />
                 </div>
             ); // creating Board components
         });
 
         return <div className="home-page">{disp}</div>; // placing them on the screen
+        }
+        
     }
 }
 
